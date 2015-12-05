a56f8b1b186aaa23c8455f2db11545449948e6f3
tulipan <modrzejewskimichal@wp.pl>
2015-12-05 22:35:57 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, c, d;
+        String b;
+        System.out.println("To jest kalkulator, policzy co chcesz");
+        System.out.println("Podaj pierwszą liczbe");
+        Scanner A = new Scanner(System.in);
+        a = A.nextInt();
+        System.out.println("Podaj operator, do wyboru: +,-,*,/");
+        Scanner B = new Scanner(System.in);
+        b = B.nextLine();
+        System.out.println("Podaj drugą liczbe");
+        Scanner C = new Scanner(System.in);
+        c = C.nextInt();
+        switch (b) {
+            case "+": {
+                d = a + c;
+                System.out.println("Twoj wynik to:" + d);
+                break;
+            }
+            case "-": {
+                d = a - c;
+                System.out.println("Twoj wynik to:" + d);
+                break;
+            }
+            case "*": {
+                d = a * c;
+                System.out.println("Twoj wynik to:" + d);
+                break;
+            }
+            case "/": {
+                if (c == 0) {
+                    System.out.println("Nie dziel przez zero!");
+                    break;
+                } else
+                    d = a / c;
+                System.out.println("Twoj wynik to:" + d);
+
+            }
+            break;
+            default: {
+                System.out.println("Niepoprawny operator");
+            }
+        }
+    }
+    // write your code here
+}
